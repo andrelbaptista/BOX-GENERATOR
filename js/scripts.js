53cc697998e3f9fb0ca65ee8a73c0b29cef9e076
Andre Baptista <122994174+andrelbaptista@users.noreply.github.com>
2024-12-09 14:51:59 -0300

    Projeto finalizado

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,11 @@ class BoxShadowGenerator {
     blurRef,
     spread,
     spreadRef,
+    color,
+    colorRef,
+    opacity,
+    opacityRef,
+    inset,
     previewBox,
     code,
     webkitCode,
@@ -21,24 +26,36 @@ class BoxShadowGenerator {
     this.blurRef = blurRef;
     this.spread = spread;
     this.spreadRef = spreadRef;
+    this.color = color;
+    this.colorRef = colorRef;
+    this.opacity = opacity;
+    this.opacityRef = opacityRef;
+    this.inset = inset;
+    this.insetRef = this.inset.checked;
     this.previewBox = previewBox;
     this.code = code;
     this.webkitCode = webkitCode;
     this.mozCode = mozCode;
   }
+
   initialize() {
     this.horizontalRef.value = this.horizontal.value;
     this.verticalRef.value = this.vertical.value;
     this.blurRef.value = this.blur.value;
     this.spreadRef.value = this.spread.value;
+    this.colorRef.value = this.color.value;
+    this.opacityRef.value = this.opacity.value;
+    
 
     this.applyRule();
     this.showCode();
   }
 
   applyRule() {
-    this.previewBox.style.boxShadow = `${this.horizontalRef.value}px ${this.verticalRef.value}px ${this.blurRef.value}px ${this.spreadRef.value}px #000000`;
-    this.currentRule = this.previewBox.style.boxShadow;
+    const rgbValue = this.hexToRgb(this.colorRef.value);
+    const shadowCode = `${this.insetRef ?  "inset" : ""} ${this.horizontalRef.value}px ${this.verticalRef.value}px ${this.blurRef.value}px ${this.spreadRef.value}px rgba(${rgbValue}, ${this.opacityRef.value})`;
+    this.previewBox.style.boxShadow = shadowCode;
+    this.currentRule = shadowCode;
   }
 
   showCode() {
@@ -61,9 +78,24 @@ class BoxShadowGenerator {
       case "spread":
         this.spreadRef.value = value;
         break;
+      case "color":
+        this.colorRef.value = value;
+        break;
+      case "opacity":
+        this.opacityRef.value = value;
+        break;
+        case "inset":
+        this.insetRef = value;
+        break;
     }
     this.applyRule();
     this.showCode();
+  }
+
+  hexToRgb(hex) {
+    return `${("0x" + hex[1] + hex[2]) | 0}, ${("0x" + hex[3] + hex[4]) | 0}, ${
+      ("0x" + hex[5] + hex[6]) | 0
+    }`;
   }
 }
 
@@ -76,6 +108,11 @@ const blur = document.querySelector("#blur");
 const blurRef = document.querySelector("#blur-value");
 const spread = document.querySelector("#spread");
 const spreadRef = document.querySelector("#spread-value");
+const color = document.querySelector("#color");
+const colorRef = document.querySelector("#color-value");
+const opacity = document.querySelector("#opacity");
+const opacityRef = document.querySelector("#opacity-value");
+const inset = document.querySelector("#inset");
 
 const previewBox = document.querySelector("#box");
 
@@ -92,11 +129,18 @@ const boxShadow = new BoxShadowGenerator(
   blurRef,
   spread,
   spreadRef,
+  color,
+  colorRef,
+  opacity,
+  opacityRef,
+  inset,
   previewBox,
   code,
   webkitCode,
   mozCode
 );
+
+console.log(BoxShadowGenerator);
 
 boxShadow.initialize();
 
@@ -120,3 +164,37 @@ spread.addEventListener("input", (e) => {
   const value = e.target.value;
   boxShadow.updateValue("spread", value);
 });
+
+color.addEventListener("input", (e) => {
+  const value = e.target.value;
+  boxShadow.updateValue("color", value);
+});
+
+opacity.addEventListener("input", (e) => {
+  const value = e.target.value;
+  boxShadow.updateValue("opacity", value);
+});
+
+inset.addEventListener("input", (e) => {
+  const value = e.target.checked;
+  boxShadow.updateValue("inset", value);
+});
+
+// copiar o código
+const codeArea = document.querySelector("#code-area");
+const codeCopy = document.querySelector("#code-copy");
+
+codeArea.addEventListener("click", () => {
+  // retira as quebras de págiona entre as linhas de código
+  const codes = codeArea.innerText.replace(/^\s*\n/gm, '');
+  // copia para o clipboard e escreve código copiado e e promise base
+  navigator.clipboard.writeText(codes).then (() => {
+    codeCopy.innerText = "Código copiado com sucesso !";
+    setTimeout(() => {
+      codeCopy.innerText="Clique no quadro acima para copiar o código";
+    },2000);
+  })
+
+});
+
+
