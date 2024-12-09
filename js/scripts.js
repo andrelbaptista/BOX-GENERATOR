a8bc484ea71cda9d7aa5d295ac600ff2e25e06d9
Andre Baptista <122994174+andrelbaptista@users.noreply.github.com>
2024-12-09 10:39:18 -0300

    html e css

---
+++ js/scripts.js
@@ -0,0 +1,84 @@
+class BoxShadowGenerator {
+  constructor(
+    horizontal,
+    horizontalRef,
+    vertical,
+    verticalRef,
+    blur,
+    blurRef,
+    spread,
+    spreadRef,
+    previewBox,
+    code,
+    webkitCode,
+    mozCode
+  ) {
+    this.horizontal = horizontal;
+    this.horizontalRef = horizontalRef;
+    this.vertical = vertical;
+    this.verticalRef = verticalRef;
+    this.blur = blur;
+    this.blurRef = blurRef;
+    this.spread = spread;
+    this.spreadRef = spreadRef;
+    this.previewBox = previewBox;
+    this.code = code;
+    this.webkitCode = webkitCode;
+    this.mozCode = mozCode;
+  }
+  initialize() {
+    this.horizontalRef.value = this.horizontal.value;
+    this.verticalRef.value = this.vertical.value;
+    this.blurRef.value = this.blur.value;
+    this.spreadRef.value = this.spread.value;
+
+    this.applyRule();
+    this.showCode();
+  }
+
+  applyRule() {
+    this.previewBox.style.boxShadow = `${this.horizontalRef.value}px ${this.verticalRef.value}px ${this.blurRef.value}px ${this.spreadRef.value}px #000000`;
+    this.currentRule = this.previewBox.style.boxShadow;
+  }
+
+  showCode() {
+    this.code.innerText = this.currentRule;
+    this.webkitCode.innerText = this.currentRule;
+    this.mozCode.innerText = this.currentRule;
+  }
+}
+
+// Seleção de elementos
+const horizontal = document.querySelector("#horizontal");
+const horizontalRef = document.querySelector("#horizontal-value");
+const vertical = document.querySelector("#vertical");
+const verticalRef = document.querySelector("#vertical-value");
+const blur = document.querySelector("#blur");
+const blurRef = document.querySelector("#blur-value");
+const spread = document.querySelector("#spread");
+const spreadRef = document.querySelector("#spread-value");
+
+const previewBox = document.querySelector("#box");
+
+const code = document.querySelector("#code span");
+const webkitCode = document.querySelector("#webkit-code span");
+const mozCode = document.querySelector("#moz-code span");
+
+const boxShadow = new BoxShadowGenerator(
+  horizontal,
+  horizontalRef,
+  vertical,
+  verticalRef,
+  blur,
+  blurRef,
+  spread,
+  spreadRef,
+  previewBox,
+  code,
+  webkitCode,
+  mozCode
+);
+
+boxShadow.initialize();
+
+// Eventos
